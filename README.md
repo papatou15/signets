91f97b0822ed3a9fb44b4c7044b543f2bc95172a
Camille Semaan <cdem@xvitae.com>
2022-03-16 17:49:31 -0400

    Démarrage intégration de Firebase

---
+++ README.md
@@ -0,0 +1,3 @@
+# Application Web Signets
+
+Exemple de classe dans le cours 582-4PA à la session Hiver 2022.
---
+++ src/composants/Appli.jsx
@@ -0,0 +1,28 @@
+import './Appli.scss';
+import Entete from './Entete';
+import ListeDossiers from './ListeDossiers';
+import Accueil from './Accueil';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import { useState, useEffect } from 'react';
+
+export default function Appli() {
+  // État 'utilisateur'
+  const [utilisateur, setUtilisateur] = useState(null);
+
+  return (
+      utilisateur ?
+        <div className="Appli">
+            <Entete />
+            <section className="contenu-principal">
+              <ListeDossiers />
+              <Fab size="large" className="ajoutRessource" color="primary" aria-label="Ajouter dossier">
+                <AddIcon />
+              </Fab>
+            </section>
+        </div>
+      :
+        <Accueil />
+  
+  );
+}
