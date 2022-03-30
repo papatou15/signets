03b3d99a4115061e1f9730eca3729e5a1ce9d8d2
Antoine Ridard <papatou15@hotmail.com>
2022-03-30 15:00:01 -0400

    Fix le bug d'affichage

--- src/code/dossier-modele.js
+++ src/code/dossier-modele.js
@@ -7,7 +7,7 @@ import { getDocs, collection } from "firebase/firestore";
  * @returns {Promise<any[]>} Promesse avec le tableau des dossiers lorsque complété
  */
 export async function lireTout(idUtilisateur){
-    getDocs(collection(bdFirestore, 'signets', idUtilisateur, 'dossiers')).then(
+    return getDocs(collection(bdFirestore, 'signets', idUtilisateur, 'dossiers')).then(
         res => res.docs.map(doc => ({id: doc.id, ...doc.data()}))
     );
 }
